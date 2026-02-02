0f78c90861dfe3c9e9dca521339afaf97215ebb1
wahyukmr <maryu9229@gmail.com>
2026-02-02 11:30:02 +0700

    chore: update CSS structure and remove Tailwind config

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import './app.css'
+
 import {
   isRouteErrorResponse,
   Links,
@@ -8,21 +10,6 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import stylesheet from "./app.css?url";
-
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-  { rel: "stylesheet", href: stylesheet },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
