a141e0eeac99f39c7b064f43998923ab1f06206c
wahyukmr <maryu9229@gmail.com>
2026-03-01 12:13:57 +0700

    chore: update prerender configuration to support multiple locales

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,19 @@
 import type { Config } from "@react-router/dev/config";
 
+const SUPPORTED_LOCALES = ["en", "id"];
+
 export default {
 	ssr: true,
 	future: {
 		v8_middleware: true,
 	},
 	// TODO: masih perlu di optimalkan untuk prerender, karena method getSTaticPaths hanya menangani static paths, sedangkan untuk dynamic paths seperti projectId dan articleId masih belum bisa di handle dengan baik.
-	prerender: async ({ getStaticPaths }) => {
-		const staticPaths = await getStaticPaths();
-		return [...staticPaths];
-	},
-	// prerender: ['/about', '/projects', '/article/1', '/project/1'],
+	prerender: [
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}`),
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}/projects`),
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}/articles`),
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}/article/1`),
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}/project/1`),
+		...SUPPORTED_LOCALES.map((locale) => `/${locale}/about`),
+	],
 } satisfies Config;
